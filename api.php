760b034f2c2705776a06fbae35e723cc8c035dda
Artur <artur@agentejo.com>
2018-11-19 03:35:21 +0100

    Some refactoring

---
+++ api.php
@@ -0,0 +1,42 @@
+<?php
+
+// Is cache request?
+if (!isset($_REQUEST['rspc'])) {
+    return;
+}
+
+$this->on('before', function() {
+
+    $hash = trim(COCKPIT_ADMIN_ROUTE.'/'.md5(serialize($_REQUEST)), '/').'.php';
+
+    if ($file = $this->path("#tmp:apicache/{$hash}")) {
+
+        $cache = include($file);
+
+        if ($cache['eol'] < time()) {
+            unlink($file);
+            return;
+        }
+
+        $this->response->body = $cache['contents'];
+        $this->response->flush();
+
+        $this->stop();
+    }
+
+}, 2000);
+
+$this->on('after', function() {
+
+    if ($this->response->status != 200) {
+        return;
+    }
+
+    $hash = trim(COCKPIT_ADMIN_ROUTE.'/'.md5(serialize($_REQUEST)), '/').'.php';
+
+    $this->filestorage->put("tmp://apicache/{$hash}", '<?php return '.var_export([
+        'eol' => (time() + $this->retrieve('config/responseCache/duration', 60)),
+        'contents' => is_object($this->response->body) ? json_decode(json_encode($this->response->body), true) : $this->response->body
+    ], true ).';');
+
+}, -2000);
--- bootstrap.php
+++ bootstrap.php
@@ -1,47 +1,30 @@
 <?php
 
-// Is REST?
-if (!COCKPIT_API_REQUEST) {
-    return;
-}
 
-// Is cache request?
-if (!isset($_REQUEST['rspc'])) {
-    return;
-}
+$this->module('simpleresponsecache')->extend([
 
-$this->on('before', function() {
+    'clear' => function($path = '') {
 
-    $hash = trim(COCKPIT_ADMIN_ROUTE.'/'.md5(serialize($_REQUEST)), '/').'.php';
+        $path = str_replace('..', '', $path);
+        $path = $this->app->path("#tmp:apicache".($path ? "/{$path}":''));
 
-    if ($file = $this->path("#tmp:apicache/{$hash}")) {
-
-        $cache = include($file);
-
-        if ($cache['eol'] < time()) {
-            unlink($file);
-            return;
+        if ($path) {
+            $this->app->helper('fs')->delete($path);
         }
-
-        $this->response->body = $cache['contents'];
-        $this->response->flush();
-
-        $this->stop();
     }
+]);
 
-}, 2000);
 
-$this->on('after', function() {
+// Is REST?
+if (COCKPIT_API_REQUEST) {
+    include(__DIR__.'/api.php');
+}
 
-    if ($this->response->status != 200) {
-        return;
-    }
+// invalidate cache on content update
+$this->on('collections.save.after', function($name) {
+    $this->module('simpleresponsecache')->clear("api/collections/get/{$name}");
+});
 
-    $hash = trim(COCKPIT_ADMIN_ROUTE.'/'.md5(serialize($_REQUEST)), '/').'.php';
-
-    $this->filestorage->put("tmp://apicache/{$hash}", '<?php return '.var_export([
-        'eol' => (time() + $this->retrieve('config/responseCache/duration', 60)),
-        'contents' => is_object($this->response->body) ? json_decode(json_encode($this->response->body), true) : $this->response->body
-    ], true ).';');
-
-}, -2000);
+$this->on('singleton.save.after', function($singleton) {
+    $this->module('simpleresponsecache')->clear("api/collections/get/{$singleton['name']}");
+});
